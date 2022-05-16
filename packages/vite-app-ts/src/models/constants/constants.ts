0b84ad28687e7dcc616b39f0c4c3bad63b43d581
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-05-16 12:01:39 +0100

    challenge completed

--- packages/vite-app-ts/src/models/constants/constants.ts
+++ packages/vite-app-ts/src/models/constants/constants.ts
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = '3ccea23c26224daf802cf31e4699d15e';
+export const INFURA_ID = '77e2c51480c04f238f44e02af0f21730';
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = 'PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8';
